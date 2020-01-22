10c54ca43b24200016e4ff983549399079fbcd73
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-01-21 16:42:29 -0800

    Memory Match - Added card matching, win, and modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
+const mainElement = document.getElementById("gameCards");
+const modalElement = document.querySelector('.modal');
 
+var firstCardClicked, secondCardClicked, firstCardClasses, secondCardClasses, maxMatches, matches;
+
+maxMatches = 9;
+matches = 0;
+
+mainElement.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  var clickedTarget = event.target;
+
+  if (clickedTarget.className.indexOf("card-back") === -1) {
+    return;
+  }
+
+  clickedTarget.classList.add("hidden");
+
+  if (!firstCardClicked) {
+    firstCardClicked = clickedTarget;
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  } else {
+    secondCardClicked = clickedTarget;
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    mainElement.removeEventListener('click', handleClick);
+    if (firstCardClasses === secondCardClasses) {
+      mainElement.addEventListener('click', handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+      matches += 1;
+      if (matches === maxMatches) {
+        modalElement.classList.remove("hidden");
+      }
+    } else {
+      setTimeout(function () {
+        firstCardClicked.classList.remove("hidden");
+        secondCardClicked.classList.remove("hidden");
+        mainElement.addEventListener('click', handleClick);
+        firstCardClicked = null;
+        secondCardClicked = null;
+      }, 1500);
+    }
+  }
+}
