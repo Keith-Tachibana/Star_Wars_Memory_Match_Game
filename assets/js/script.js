610c91fbd1f901b063d79f5aafa5bdd4f47cbb0e
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-01-21 17:57:04 -0800

    Memory Match Finished Game Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 const mainElement = document.getElementById("gameCards");
 const modalElement = document.querySelector('.modal');
+const resetButton = document.querySelector('.reset');
+const gamesScore = document.querySelector('.games-score');
+const attemptsScore = document.querySelector('.attempts-score');
+const accuracyScore = document.querySelector('.accuracy-score');
 
-var firstCardClicked, secondCardClicked, firstCardClasses, secondCardClasses, maxMatches, matches;
+var firstCardClicked, secondCardClicked, firstCardClasses, secondCardClasses, maxMatches, matches, gamesPlayed, attempts, accuracy;
 
+accuracy = 0;
+attempts = 0;
 maxMatches = 9;
 matches = 0;
+gamesPlayed = 0;
 
 mainElement.addEventListener('click', handleClick);
 
@@ -25,14 +32,24 @@ function handleClick(event) {
     secondCardClasses = secondCardClicked.previousElementSibling.className;
     mainElement.removeEventListener('click', handleClick);
     if (firstCardClasses === secondCardClasses) {
+      attempts += 1;
+      attemptsScore.textContent = attempts;
       mainElement.addEventListener('click', handleClick);
       firstCardClicked = null;
       secondCardClicked = null;
       matches += 1;
+      accuracy = ((matches / attempts) * 100).toFixed(2);
+      accuracyScore.textContent = accuracy+'%';
       if (matches === maxMatches) {
         modalElement.classList.remove("hidden");
+        gamesPlayed += 1;
+        gamesScore.textContent = gamesPlayed;
       }
     } else {
+      attempts += 1;
+      attemptsScore.textContent = attempts;
+      accuracy = ((matches / attempts) * 100).toFixed(2);
+      accuracyScore.textContent = accuracy+'%';
       setTimeout(function () {
         firstCardClicked.classList.remove("hidden");
         secondCardClicked.classList.remove("hidden");
@@ -43,3 +60,22 @@ function handleClick(event) {
     }
   }
 }
+
+window.addEventListener('click', function(event) {
+  if (event.target === modalElement) {
+    modalElement.classList.add("hidden");
+  }
+});
+
+resetButton.addEventListener('click', function() {
+  matches = 0;
+  attempts = 0;
+  attemptsScore.textContent = 0;
+  accuracy = 0;
+  accuracyScore.textContent = 0+'%';
+  var cardsHidden = document.querySelectorAll('.hidden');
+  for (var i = 0; i < cardsHidden.length; i++) {
+    cardsHidden[i].classList.remove("hidden");
+  }
+  modalElement.classList.add("hidden");
+})
