b555c82ae3992d5e2470a120eb0101847941e5ec
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-01-21 22:27:21 -0800

    Memory Match - Finished random function for shuffle

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,12 @@ const resetButton = document.querySelector('.reset');
 const gamesScore = document.querySelector('.games-score');
 const attemptsScore = document.querySelector('.attempts-score');
 const accuracyScore = document.querySelector('.accuracy-score');
+const cardFront = document.querySelectorAll('.card-front');
+const shuffleButton = document.querySelector('.shuffle-button');
 
 var firstCardClicked, secondCardClicked, firstCardClasses, secondCardClasses, maxMatches, matches, gamesPlayed, attempts, accuracy;
+
+var cardDeck = ['js-logo', 'js-logo', 'html-logo', 'html-logo', 'github-logo', 'github-logo', 'docker-logo', 'docker-logo', 'css-logo', 'css-logo', 'mysql-logo', 'mysql-logo', 'node-logo', 'node-logo', 'php-logo', 'php-logo', 'react-logo', 'react-logo'];
 
 accuracy = 0;
 attempts = 0;
@@ -14,6 +18,8 @@ matches = 0;
 gamesPlayed = 0;
 
 mainElement.addEventListener('click', handleClick);
+shuffleButton.addEventListener('click', shuffleCards);
+resetButton.addEventListener('click', shuffleCards);
 
 function handleClick(event) {
   var clickedTarget = event.target;
@@ -61,21 +67,25 @@ function handleClick(event) {
   }
 }
 
-window.addEventListener('click', function(event) {
-  if (event.target === modalElement) {
-    modalElement.classList.add("hidden");
+function resetCards () {
+  cardFront.forEach(element => element.removeAttribute('class'));
+  var newCardDeck = cardDeck.sort(function() { return 0.5-Math.random() });
+  for (var i = 0; i < newCardDeck.length; i++) {
+    cardFront.forEach(element => element.setAttribute('class', 'card-front'));
+    cardFront.forEach(element => element.classList.add(newCardDeck[i]));
   }
-});
+}
 
-resetButton.addEventListener('click', function() {
+function shuffleCards () {
   matches = 0;
   attempts = 0;
   attemptsScore.textContent = 0;
   accuracy = 0;
-  accuracyScore.textContent = 0+'%';
+  accuracyScore.textContent = 0 + '%';
   var cardsHidden = document.querySelectorAll('.hidden');
   for (var i = 0; i < cardsHidden.length; i++) {
     cardsHidden[i].classList.remove("hidden");
   }
   modalElement.classList.add("hidden");
-})
+  resetCards();
+}
